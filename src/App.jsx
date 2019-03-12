a91490e1a9812defd53a2ce0bf00cb8a3944dc70
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-03-12 22:13:25 +0000

    connected messages to messagelist database

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,17 @@ import NavBar from './NavBar.jsx'
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      loading: true
-    }
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({loading: false});
-    }, 3000)
-  }
+  }  
 
   render() {
-    if(this.state.loading === true) {
-      return (
-        <h1>loading...</h1>
-      );
-    } else {
-    return ( <div>
-        <NavBar />
-        <Messages />
-        <ChatBar />
-      </div>);
-    }
+
+    return ( 
+          <div>
+            <NavBar />
+            <Messages />
+            <ChatBar />
+          </div>
+          );
   }
 }
 export default App;
--- src/Messages.jsx
+++ src/Messages.jsx
@@ -1,13 +1,47 @@
 import React, {Component} from 'react';
+import messages from './messageList';
+import { generateRandomId } from './utils';
+
+function MessageItem(props) {
+      return (
+        <div>
+            <div className="message">
+                <span className="message-username">{props.message.username}</span>
+                <span className="message-content">{props.message.content}</span>
+            </div>
+        </div>
+      );
+}
 
 class Messages extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            messages
+        };
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            console.log("Simulating incoming message");
+            // Add a new message to the list of messages in the data store
+            const newMessage = {username: "Michelle", content: "Hello there!", ID: "asdasdqwdasdsa"};
+            const messages = this.state.messages.concat(newMessage)
+            // Update the state of the app component.
+            // Calling setState will trigger a call to render() in App and all child components.
+            this.setState({messages: messages})
+          }, 3000);
+      }
+
     render() {
+        const messageItem = this.state.messages.map(message => {
+           return <MessageItem message = {message} key={message.ID}/>
+        });
+
         return (
             <main className="messages">
-                <div className="message">
-                    <span className="message-username">Anonymous1</span>
-                    <span className="message-content">I won't be impressed with technology until I can download food.</span>
-                 </div>
+                {messageItem}
                 <div className="message system">
                     Anonymous1 changed their name to nomnom.
                 </div>
