240b07ad65c046e233566fd20031679d396ef090
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-03-12 18:38:58 +0000

    added basic template and implemented base components into app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import Messages from './Messages.jsx'
+import NavBar from './NavBar.jsx'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({loading: false});
+    }, 3000)
+  }
+
   render() {
-    return (
-      <h1>Hello React :)</h1>
-    );
+    if(this.state.loading === true) {
+      return (
+        <h1>loading...</h1>
+      );
+    } else {
+    return ( <div>
+        <NavBar />
+        <Messages />
+        <ChatBar />
+      </div>);
+    }
   }
 }
 export default App;
